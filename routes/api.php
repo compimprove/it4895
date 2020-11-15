aa31c0ca7761e09ce4f5a35da41cd5e68431a318
DinhNT <compimprove@gmail.com>
2020-11-15 18:33:10 +0700

    make api page

--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::middleware(['auth:sanctum', 'user-blocked'])->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
     Route::post('/user/settings', "SettingsController@setPushSetting")->name("set_push_settings");
-    Route::get('/user/notifications', "UserController@getNotifications")->name("get_notifications");
+    Route::get('/user/notifications', "UserController@getNotifications")->name("get_notification");
     Route::post('/user/notifications', "UserController@setReadNotification")->name("set_read_notification");
     Route::get('/user/settings', "SettingsController@getPushSetting")->name("get_push_settings");
     Route::get('/user/requested-friends', "UserController@getRequestedFriends")->name("get_requested_friends");
@@ -28,11 +27,11 @@ Route::middleware(['auth:sanctum', 'user-blocked'])->group(function () {
     Route::get('/user/friends', "UserController@getFriends")->name("get_user_friends");
     Route::get('/user/suggested-friends', "UserController@getSuggestedFriends")->name("get_list_suggested_friends");
     Route::post('/user/friends', "UserController@setFriends")->name("set_accept_friend");
-    Route::post("/logout", 'AuthController@logout');
-    Route::post("/change-password", "AuthController@changePassword");
-    Route::post("/device", "DeviceController@setDeviceInfo");
+    Route::post("/logout", 'AuthController@logout')->name("Logout");
+    Route::post("/change-password", "AuthController@changePassword")->name("change_password");
+    Route::post("/device", "DeviceController@setDeviceInfo")->name("set_devtoken");
     Route::get("user/block", "UserController@getBlock")->name("get_list_blocks");
-    Route::get("/user/{id}", "UserController@getInfo");
+    Route::get("/user/{id}", "UserController@getInfo")->name("get_user_info");
     Route::post('change-info-after-signup', 'UserController@changeInfoAfterSignup')->name("change_info_after_signup");
     Route::post("/set-user-info", "UserController@setUserInfo")->name("set_user_info");
     Route::get('messages/{userId2}', 'ChatController@fetchAllMessages');
@@ -44,14 +43,16 @@ Route::middleware(['auth:sanctum', 'user-blocked'])->group(function () {
     Route::post("user/block/{user_id}", "UserController@setBlock")->name("set_block");
 });
 
-Route::post('login', 'AuthController@getToken');
-Route::post('register', 'AuthController@register');
+Route::post('login', 'AuthController@getToken')->name("Login");
+Route::post('register', 'AuthController@register')->name("Signup");
 Route::post('check-verify-code', 'AuthController@checkVerifyCode')->name("check_verify_code");
+
 Route::post('testSaveFile', 'UserController@testSaveFile');
 Route::post('testDeleteFile', 'UserController@testDeleveFile');
-Route::post('post/add', 'PostController@addPost');
-Route::get('post/{id}', 'PostController@getPost');
-Route::get('post/delete/{id}', 'PostController@deletePost');
+
+Route::post('post/add', 'PostController@addPost')->name("add_post");
+Route::get('post/{id}', 'PostController@getPost')->name("get_post");
+Route::get('post/delete/{id}', 'PostController@deletePost')->name("delete_post");
 //Route::post('post/addComment/{id}','PostController@addComment');
 
 
