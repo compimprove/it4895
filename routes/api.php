bf62c4527367d1f6cb99a10c46342d525ec63bc8
DinhNT <compimprove@gmail.com>
2020-12-17 18:11:17 +0700

    .

--- routes/api.php
+++ routes/api.php
@@ -18,21 +18,21 @@ Route::middleware(['attach-token', 'auth:sanctum', 'user-blocked', 'validate-res
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    Route::post('/user/settings', "SettingsController@setPushSetting")->name("set_push_settings");
-    Route::get('/user/notifications', "UserController@getNotifications")->name("get_notification");
-    Route::post('/user/notifications', "UserController@setReadNotification")->name("set_read_notification");
-    Route::get('/user/settings', "SettingsController@getPushSetting")->name("get_push_settings");
-    Route::get('/user/get_requested_friends', "UserController@getRequestedFriends")->name("get_requested_friends");
-    Route::post('/user/set_request_friend', "UserController@setRequestFriends")->name("set_request_friend");
-    Route::get('/user/friends', "UserController@getFriends")->name("get_user_friends");
-    Route::get('/user/get_list_suggested_friends', "UserController@getSuggestedFriends")->name("get_list_suggested_friends");
-    Route::post('/user/friends', "UserController@setFriends")->name("set_accept_friend");
+    Route::post('/set_push_settings', "SettingsController@setPushSetting")->name("set_push_settings");
+    Route::get('/get_notification', "UserController@getNotifications")->name("get_notification");
+    Route::post('/set_read_notification', "UserController@setReadNotification")->name("set_read_notification");
+    Route::get('/get_push_settings', "SettingsController@getPushSetting")->name("get_push_settings");
+    Route::get('/get_requested_friends', "UserController@getRequestedFriends")->name("get_requested_friends");
+    Route::post('/set_request_friend', "UserController@setRequestFriends")->name("set_request_friend");
+    Route::get('/get_user_friends', "UserController@getFriends")->name("get_user_friends");
+    Route::get('/get_list_suggested_friends', "UserController@getSuggestedFriends")->name("get_list_suggested_friends");
+    Route::post('/set_accept_friend', "UserController@setFriends")->name("set_accept_friend");
     Route::post("/logout", 'AuthController@logout')->name("Logout");
     Route::post("/change_password", "AuthController@changePassword")->name("change_password");
-    Route::post("/device", "DeviceController@setDeviceInfo")->name("set_devtoken");
-    Route::get("user/block", "UserController@getBlock")->name("get_list_blocks");
-    Route::get("/user", "UserController@getInfo")->name("get_user_info");
-    Route::post('change_info_after_signup', 'UserController@changeInfoAfterSignup')->name("change_info_after_signup");
+    Route::post("/set_devtoken", "DeviceController@setDeviceInfo")->name("set_devtoken");
+    Route::get("/get_list_blocks", "UserController@getBlock")->name("get_list_blocks");
+    Route::get("/get_user_info", "UserController@getInfo")->name("get_user_info");
+    Route::post('/change_info_after_signup', 'UserController@changeInfoAfterSignup')->name("change_info_after_signup");
 
     Route::post('add_post', 'PostController@addPost')->name("add_post");
     Route::post('edit_post', 'PostController@editPost')->name("edit_post");
@@ -43,18 +43,20 @@ Route::middleware(['attach-token', 'auth:sanctum', 'user-blocked', 'validate-res
 
     Route::post("/set_user_info", "UserController@setUserInfo")->name("set_user_info");
 
-    Route::get('messages/get_list_conversation', 'ChatController@getListConversation')->name("get_list_conversation");
-    Route::get('messages/conversation', 'ChatController@getConversation')->name("get_conversation");
-    Route::post('messages/set_read_message', 'ChatController@setReadMessage')->name("set_read_message");
-    Route::post('messages/delete_message', 'ChatController@deleteMessage')->name("delete_message");
-    Route::post('messages/delete_conversation', 'ChatController@deleteConversation')->name("delete_conversation");
+    Route::get('/get_list_conversation', 'ChatController@getListConversation')->name("get_list_conversation");
+    Route::get('/get_conversation', 'ChatController@getConversation')->name("get_conversation");
+    Route::post('/set_read_message', 'ChatController@setReadMessage')->name("set_read_message");
+    Route::post('/delete_message', 'ChatController@deleteMessage')->name("delete_message");
+    Route::post('/delete_conversation', 'ChatController@deleteConversation')->name("delete_conversation");
+
     Route::get('message', 'ChatController@fetchAllMessages');
     Route::post('message', 'ChatController@sendMessage');
-    Route::post('report', 'UserReportPostController@reportPost')->name("report_post");
-    Route::post('like/add', 'UserLikePostController@likePost')->name("like_post");
-    Route::get('comment', 'CommentController@getComment')->name("get_comment");
-    Route::post('comment/add', 'CommentController@addComment')->name("set_comment");
-    Route::post("user/block", "UserController@setBlock")->name("set_block");
+
+    Route::post('report_post', 'UserReportPostController@reportPost')->name("report_post");
+    Route::post('/like_post', 'UserLikePostController@likePost')->name("like_post");
+    Route::get('/get_comment', 'CommentController@getComment')->name("get_comment");
+    Route::post('/set_comment', 'CommentController@addComment')->name("set_comment");
+    Route::post("/set_block", "UserController@setBlock")->name("set_block");
 
     Route::post('search', 'SearchController@search')->name("search");
     Route::post('get_saved_search', 'SearchController@getSavedSearch')->name("get_saved_search");
